063c57ee9d4551f5cdee75e5f1e363233a369aa6
laurent <laniakea.sgaetoile@gmx.fr>
2021-07-01 00:51:59 +0200

    Table de multiplication au hazard

--- multi.py
+++ multi.py
@@ -1,10 +1,13 @@
+from random import randint as rd
+n = rd(1,50000)
+max = rd(5,50)
+
 def multi(n, max):
-    ### Renvoi la table de n jusqu'à i ###
+    ### Renvoi la table de n  jusqu'à i au hazard ###
     i = 1
     while i <= max:
         print(n, "x", i, "=", n * i)
         i += 1
-n = 7
-max = 15
-print("Table de", n, ":")
+
+print("Table de", n,"par", max, ":")
 print(multi(n, max))
