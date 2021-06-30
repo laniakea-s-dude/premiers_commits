86fc3505fa504f138fdaf2aec8001e3dc009ac86
laurent <laniakea.sgaetoile@gmx.fr>
2021-06-30 23:15:39 +0200

    Table de multiplication - Début

---
+++ multi.py
@@ -0,0 +1,9 @@
+def multi(n, max):
+    ### Renvoi la table de n jusqu'à i ###
+    i = 0
+    while i < max:
+        print(n, "x", i, "=", n * i)
+        i += 1
+n = 7
+max = 15
+print(multi(n, max))
